d7b805928a23bc60e151286e0a053fccac47a60c
omary <omary@OMAZING51>
2025-08-09 20:53:02 -0500

    feat(ui): add prototype card component - Create placeholder card with example data - Basic responsive layout

--- frontend/funds_frontend/src/App.jsx
+++ frontend/funds_frontend/src/App.jsx
@@ -41,8 +41,28 @@ function App() {
             <h1 className='text-2xl font-bold'>Dashboard</h1>
             <div className='bg-gray-300 w-10 h-10 rounded-full'></div>
           </header>
-          <div>
-            
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4'>
+           <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 p-4 gap-2'>
+            <div className='bg-white p-6 shadow-lg rounded-lg'>
+              <h2 className='text-xl font-bold'>Card</h2>
+              <p>Ejemplo jejejejejejejejejejejejejejejejejejejejejejejejje</p>
+            </div>
+            <button className='bg-red-400 text-md font-bold rounded-sm p-2'>Eliminar fondo</button>
+           </div>
+           <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 p-4 gap-2'>
+            <div className='bg-white p-6 shadow-lg rounded-lg'>
+              <h2 className='text-xl font-bold'>Card</h2>
+              <p>Ejemplo jejejejejejejejejejejejejejejejejejejejejejejejje</p>
+            </div>
+            <button className='bg-red-400 text-md font-bold rounded-sm p-2'>Eliminar fondo</button>
+           </div>
+           <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 p-4 gap-2'>
+            <div className='bg-white p-6 shadow-lg rounded-lg'>
+              <h2 className='text-xl font-bold'>Card</h2>
+              <p>Ejemplo jejejejejejejejejejejejejejejejejejejejejejejejje</p>
+            </div>
+            <button className='bg-red-400 text-md font-bold rounded-sm p-2'>Eliminar fondo</button>
+           </div>
           </div>
         </main>
       </div>
